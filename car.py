fccc9bd3046d02a1a659817c20997f49daa859de
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2021-02-25 21:48:48 +0200

    added car.py

---
+++ car.py
@@ -0,0 +1,5 @@
+from street import Street
+
+class Car:
+    def __init__(self, streets_lst: list):
+        self.streets_lst = streets_lst
--- street.py
+++ street.py
@@ -1,12 +1,11 @@
 class Street:
-    def __init__(self, start, end, name, time_to_cross, queue_length=0):
+    def __init__(self, start, end, name, time_to_cross, queue):
         self.start = start
         self.end = end
         self.name = name
         self.time_to_cross = time_to_cross
-        self.queue_length = queue_length
+        self.queue = queue
 
-            
 
 
 def get_perfect_time(streets):
