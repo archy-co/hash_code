a6515725706ed27bbbdb951596af825bcbf3f3d3
archy-co <yaroslav.moskalyk@ucu.edu.ua>
2021-02-25 20:46:46 +0200

    Add steet.py

---
+++ street.py
@@ -0,0 +1,9 @@
+class Street:
+    def __init__(self, start, end, name, time_to_cross):
+        self.start = start
+        self.end = end
+        self.name = name
+        self.time_to_cross = time_to_cross
+
+
+
