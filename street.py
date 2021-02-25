fd9076601b57f58464409485337a6d9a4cb592dc
archy-co <yaroslav.moskalyk@ucu.edu.ua>
2021-02-25 21:37:42 +0200

    add queue_length attribute

--- street.py
+++ street.py
@@ -1,9 +1,10 @@
 class Street:
-    def __init__(self, start, end, name, time_to_cross):
+    def __init__(self, start, end, name, time_to_cross, queue_length=0):
         self.start = start
         self.end = end
         self.name = name
         self.time_to_cross = time_to_cross
+        self.queue_length = queue_length
 
             
 
@@ -14,5 +15,4 @@ def get_perfect_time(streets):
         perfect_time += street.time_to_cross
 
     return perfect_time
-
 
